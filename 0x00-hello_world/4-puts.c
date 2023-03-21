a015744dc6b9382936523475cc97f2dfe9e3a382
Wisani <matobelaw@gmail.com>
2023-03-21 02:57:22 -0700

    deleted duplicates and fixed advanced task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
+// the function below prints a string 
 
-int main(void){
-  puts("Programming is like building a multilingual puzzle""\n");
 
-  return (0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }
