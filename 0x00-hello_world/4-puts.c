96063cbc0af6583ca50ac3d2888a0e85e09d18d5
Wisani <matobelaw@gmail.com>
2023-03-21 04:14:50 -0700

    fixed betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-// the function below prints a string 
-
+/** the function below prints a string                                                                                                               
+ *main - point of entry                                                                                                                              
+ *Return:0                                                                                                                                           
+ */
 
 int main(void)
 {
